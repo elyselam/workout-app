5ed5e355c93ba04f1142b70c141b096313de0414
elyselam <elyse.p.lam@gmail.com>
2021-10-16 19:39:49 -0700

    mysql changes

--- 01-starter-files/db-scripts/02-create-data.sql
+++ 01-starter-files/db-scripts/02-create-data.sql
@@ -1,10 +1,10 @@
 -- -----------------------------------------------------
 -- Schema workout
 -- -----------------------------------------------------
-DROP SCHEMA IF EXISTS `workout`;
+DROP SCHEMA IF EXISTS `workoutapp`;
 
-CREATE SCHEMA `workout`;
-USE `workout` ;
+CREATE SCHEMA `workoutapp`;
+USE `workoutapp` ;
 
 
 -- -----------------------------------------------------
@@ -12,7 +12,7 @@ USE `workout` ;
 -- 1 user, 6 routines
 -- user_id is the contraint on routines, can't delete user
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `workout`.`user_table` (
+CREATE TABLE IF NOT EXISTS `workoutapp`.`user_table` (
   `user_id` INT(11) NOT NULL AUTO_INCREMENT,
   `user_name` VARCHAR(255) NULL DEFAULT NULL,
   PRIMARY KEY (`user_id`))
@@ -25,7 +25,7 @@ AUTO_INCREMENT = 1;
 -- 1 routine, 6 exercises (i'm gonna start with just 1 for now)
 -- routine_name: [push1, pull1, hybrid1, push2, pull2, hybrid2]
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `workout`.`routine_table` (
+CREATE TABLE IF NOT EXISTS `workoutapp`.`routine_table` (
   `routine_id` INT(11) NOT NULL AUTO_INCREMENT,
   `routine_name` VARCHAR(255) NULL DEFAULT NULL,
   PRIMARY KEY (`routine_id`))
@@ -35,23 +35,24 @@ AUTO_INCREMENT = 1;
 -- -----------------------------------------------------
 -- Table `workout`.`exercise_name_table`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `workout`.`exercise_name_table` (
+CREATE TABLE IF NOT EXISTS `workoutapp`.`exercise_name_table` (
   `exercise_id` INT(11) NOT NULL AUTO_INCREMENT,
   `exercise_name` VARCHAR(255) DEFAULT NULL,
+  `routine_day` INT(11) NOT NULL,
   `set_1` INT(11) DEFAULT NULL,
   `set_2` INT(11) DEFAULT NULL,
   `set_3` INT(11) DEFAULT NULL,
   `set_4` INT(11) DEFAULT NULL,
    `reps_accomplished` BOOLEAN,
     PRIMARY KEY (`exercise_id`),
-  KEY `fk_routine` (`routine_id`),
-  CONSTRAINT `fk_routine` FOREIGN KEY (`routine_id`) REFERENCES `routine_table` (`routine_id`)
+  KEY `fk_routine` (`routine_day`),
+  CONSTRAINT `fk_routine` FOREIGN KEY (`routine_day`) REFERENCES `routine_table`(`routine_id`)
 ) 
 ENGINE=InnoDB
 AUTO_INCREMENT = 1;
 
 
-
+-- SHOW FULL COLUMNS FROM routine_table;
 
 -- -----------------------------------------------------
 --insert
