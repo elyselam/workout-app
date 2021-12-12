81c6ea99069364d10571ede0cdbb7648414d9076
elyselam <elyse.p.lam@gmail.com>
2021-12-11 17:45:12 -0800

    added input tables for each exercise

--- 03-frontend/angular-workout/src/app/common/exercise.ts
+++ 03-frontend/angular-workout/src/app/common/exercise.ts
@@ -3,9 +3,15 @@ export class Exercise {
   exerciseName: string;
   startingWeight: number;
   incWeightBy: number;
+  minRep: number;
+  maxRep: number;
   set1: number;
   set2: number;
   set3: number;
   set4: number;
   repsAccomplished: boolean;
+
+  constructor() {
+
+  }
 }
